9d26c736085346c0f8c83d1050f34108367f7c3a
Leonardo <leonardogeminiani7@gmail.com>
2023-12-05 15:20:53 +0100

    add User.csv

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("sas");
+﻿using(var db = new UserContext())
+{
+    using(var reader = new StreamReader("./Users.csv")){
+        _ = reader.ReadLine();
+        while (!reader.EndOfStream){
+            var line = reader.ReadLine();
+            var values = line!.Split(',');
+            
+            db.Add(new User(){
+                NickName = values[0],
+                Password = values[2],
+                GamesPlayed = Convert.ToUInt32(values[3]),
+                Points = Convert.ToUInt32(values[4]),
+                SSO = Convert.ToUInt32(values[5])
+            });
+        }
+    }
+
+    db.SaveChanges();
+}
